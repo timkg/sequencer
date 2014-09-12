61b5e3c94922ff3c7e7324efcc10612a86bb214d
timkg <tim.theodor@gmail.com>
2014-09-12 03:07:32 +0200

    added Tree data structure which uses Node internally

---
+++ src/tree.js
@@ -0,0 +1,47 @@
+var extend = require('util-extend');
+var LevelOrderIterator = require('../src/levelOrderIterator');
+var Node = require('../src/node');
+
+function Tree (json) {
+  this.root = buildTreeNodesFromJson(json)[0];
+}
+
+extend(Tree.prototype, {
+
+});
+
+function buildTreeNodesFromJson (json) {
+  // first we create an array of Node instances
+  var nodes = [], nodeInstanceCreator;
+  nodeInstanceCreator = new LevelOrderIterator(function (jsonNode) {
+    var n, props = {};
+
+    Object.keys(jsonNode).forEach(function (propertyName) {
+      if (propertyName !== 'children') {
+        props[propertyName] = jsonNode[propertyName];
+      }
+    });
+
+    n = new Node(props);
+    n._childrenLength = jsonNode.children.length;
+
+    nodes.push(n);
+  });
+  nodeInstanceCreator.start(json);
+
+  // then we assemble those into parent-child relationship
+  var count = 0;
+  nodes.forEach(function (node) {
+    var i = 0;
+    while (i < node._childrenLength) {
+      i++;
+      node.addChild(nodes[count + i]);
+    }
+    count += node.children.length;
+    delete node._childrenLength;
+  });
+
+  return nodes;
+}
+
+module.exports = Tree;
---
+++ test/tree_test.js
@@ -0,0 +1,78 @@
+var assert = require('assert');
+var Tree = require('../src/tree');
+var Node = require('../src/node');
+var Iterator = require('../src/iterator');
+
+assert.ok(Tree);
+
+var treeData = {
+  root: {
+    name: 'lesson',
+    children: [
+      {
+        name: 'trainer 1',
+        children: [
+          {
+            name: 'trainer 1 - item 1',
+            children: [
+              {
+                name: 'trainer 1 - item 1 - gap 1',
+                children: []
+              },
+              {
+                name: 'trainer 1 - item 2 - gap 2',
+                children: []
+              }
+            ]
+          },
+          {
+            name: 'trainer 1 - item 2',
+            children: []
+          }
+        ]
+      },
+      {
+        name: 'trainer 2',
+        children: [
+          {
+            name: 'trainer 2 - item 1',
+            children: []
+          },
+          {
+            name: 'trainer 2 - item 2',
+            children: []
+          },
+          {
+            name: 'trainer 2 - item 3',
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+};
+
+var tree = new Tree(treeData);
+
+var visitedNodes = [];
+var iterator = new Iterator(function (node) {
+  assert(node.constructor === Node);
+  visitedNodes.push(node.name);
+});
+
+iterator.start(tree);
+
+assert.deepEqual(visitedNodes, [
+  'lesson',
+  'trainer 1',
+  'trainer 1 - item 1',
+  'trainer 1 - item 1 - gap 1',
+  'trainer 1 - item 2 - gap 2',
+  'trainer 1 - item 2',
+  'trainer 2',
+  'trainer 2 - item 1',
+  'trainer 2 - item 2',
+  'trainer 2 - item 3'
+]);
+
+console.log('ok');
