2a675fe67d01a1e53f02ac8b9378914cf7366530
timkg <tim.theodor@gmail.com>
2014-09-14 14:32:23 +0200

    renamed asyncIterator to test/asyncPreOrderIterator_test.js

---
+++ test/asyncPreOrderIterator_test.js
@@ -0,0 +1,79 @@
+var assert = require('assert');
+var AsyncPreOrderIterator = require('../src/asyncPreOrderIterator');
+var Tree = require('../src/tree');
+
+assert.ok(AsyncPreOrderIterator);
+
+var treeData = {
+  root: {
+    name: 'lesson',
+    children: [
+      {
+        name: 'trainer 1',
+        children: [
+          {
+            name: 'trainer 1 - item 1',
+            children: [
+              {
+                name: 'trainer 1 - item 1 - gap 1',
+                children: []
+              },
+              {
+                name: 'trainer 1 - item 2 - gap 2',
+                children: []
+              }
+            ]
+          },
+          {
+            name: 'trainer 1 - item 2',
+            children: []
+          }
+        ]
+      },
+      {
+        name: 'trainer 2',
+        children: [
+          {
+            name: 'trainer 2 - item 1',
+            children: []
+          },
+          {
+            name: 'trainer 2 - item 2',
+            children: []
+          },
+          {
+            name: 'trainer 2 - item 3',
+            children: []
+          }
+        ]
+      }
+    ]
+  }
+};
+
+var tree = new Tree(treeData);
+
+var visitedNodes = [];
+var iterator = new AsyncPreOrderIterator(onVisit, onEnd);
+iterator.start(tree);
+
+function onVisit(node, cb) {
+  visitedNodes.push(node.name);
+  process.nextTick(cb);
+}
+
+function onEnd () {
+  assert.deepEqual(visitedNodes, [
+    'lesson',
+    'trainer 1',
+    'trainer 1 - item 1',
+    'trainer 1 - item 1 - gap 1',
+    'trainer 1 - item 2 - gap 2',
+    'trainer 1 - item 2',
+    'trainer 2',
+    'trainer 2 - item 1',
+    'trainer 2 - item 2',
+    'trainer 2 - item 3'
+  ]);
+  console.log('ok');
+}
