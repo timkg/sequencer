b92cfb299970bb3cfcce7e52a3a0102a8e546e40
timkg <tim.theodor@gmail.com>
2014-11-17 23:55:31 +0100

    fixed Step constructor comment header

--- src/flow/step.js
+++ src/flow/step.js
@@ -5,7 +5,7 @@ var Promise = require('es6-promise').Promise;
  * * A unit of operation which might be asynchronous.
  * Can be chained with other Steps to form a Sequence.
  * cb must accept Promise-compliant fulfill handler.
- * @param cb {Promise}
+ * @param cb {function(fulfill, reject)}
  * @constructor
  */
 function Step (cb) {
